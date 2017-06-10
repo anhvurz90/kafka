1557d53fa6bc6512043be9279c725972d50b1fc2
anhvurz90 <anhvurz90@gmail.com>
2017-06-10 11:38:02 +0700

    Add Tutorialspoint: Apache-Kafka: Basic Operations

--- kafka.java
+++ kafka.java
@@ -30,5 +30,41 @@ Apache Kafka
 --from-beginning
   }
   3.2.Single Node - Multiple Brokers Configuration: {
+    - Clone from config/server.properties:
+      + config/server-one.properties {
+          broker.id=1
+          port=9093
+          log.dirs=/tmp/kafka-logs-1
+      }
+      + config/server-two.properties {
+          broker.id=2
+          port=9094
+          log.dirs=/tmp/kafka-logs-2
+      }
+    - Start Multiple Brokers: {
+      bin/kafka-server-start.sh conf/server.properties
+      bin/kafka-server-start.sh conf/server-one.properties
+      bin/kafka-server-start.sh conf/server-two.properties
+    }
+    - Create a Topic: {
+      bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partition 1 --topic MultiBrokerApplication
+    }
+    - Describe a Topic: {
+      bin/kafka-topics.sh --describe --zookeeper localhost:2181 --topic MultiBrokerApplication
+    }
+    - Start Producer to Send Messages: {
+      bin/kafka-console-producer.sh --broker-list localhost:9092 --topic MultiBrokerApplication
+    }
+    - Start Consumer to Receive Messages: {
+      bin/kafka-console-consumer.sh --zookeeper localhost:2181 --topic MultiBrokerApplication --from-beginning
+    }
+  }
+  3.3.Basic Topic Operations: {
+    - Modify a Topic: {
+      bin/kafka-topics.sh --zookeeper localhost:2181 --alter --topic Hello-kafka --partitions 2
+    }
+    - Deleting a Topic: {
+      bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic Hello-kafka
+    }
   }
 }
